0199291a9137e4f76520f19fe812c1de759e9e7f
chenyiming <jinjinanan@hotmail.com>
2016-07-29 17:25:47 +0800

    添加验证输入框

---
+++ Object-C/验证码输入框/VerifyCodeInputBox/VerifyCodeInputBox/InputBox/InputView.m
@@ -0,0 +1,23 @@
+//
+//  InputView.m
+//  VerifyCodeInputBox
+//
+//  Created by JSB_02 on 16/7/29.
+//  Copyright © 2016年 JSB_02. All rights reserved.
+//
+
+#import "InputView.h"
+
+@implementation InputView
+
+
+
+-(id)initWithFrame:(CGRect)frame{
+    self = [super initWithFrame:frame];
+    if (self) {
+        
+    }
+    return self;
+}
+
+@end
